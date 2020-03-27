c721018f2ed8ae3f1ff50ed8add807575ef37a5c
cristi-maldonado <48282706+cristi-maldonado@users.noreply.github.com>
2020-03-27 14:51:16 -0400

    Update cookie.py

--- code/cookie.py
+++ code/cookie.py
@@ -8,12 +8,20 @@ License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 from thinkbayes import Pmf
 
 pmf = Pmf()
+#the probability that you will pick from bowl 1 or 2 
+#half as likely for both so .5 and .5
+#prior distribution : contains priors for the hypothesis
 pmf.Set('Bowl 1', 0.5)
 pmf.Set('Bowl 2', 0.5)
 
+#update the distribution based on new data(vanilla cookie)
+#multiply each prior by corresponding likelihood
 pmf.Mult('Bowl 1', 0.75)
 pmf.Mult('Bowl 2', 0.5)
 
+#need to renormalize- hypothesis are mutually exclusive//collectively exhaustive
+#does a posterior distribution 
 pmf.Normalize()
 
+#now you can get posterior probability!
 print pmf.Prob('Bowl 1')
